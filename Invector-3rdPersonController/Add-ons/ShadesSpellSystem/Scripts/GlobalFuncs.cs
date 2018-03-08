bc710ca40c9f3b0a8f9f01e7b1b8e0cac5d050f7
Rob Alexander <mr_rob_alexander@protonmail.com>
2018-03-08 23:12:59 +0000

    added class clone function

--- Invector-3rdPersonController/Add-ons/ShadesSpellSystem/Scripts/GlobalFuncs.cs
+++ Invector-3rdPersonController/Add-ons/ShadesSpellSystem/Scripts/GlobalFuncs.cs
@@ -598,6 +598,33 @@ namespace Shadex
 
 
         }
+
+        /// <summary>
+        /// Shallow copy one class to another.
+        /// </summary>
+        /// <param name="dst">Destination class.</param>
+        /// <param name="src">Source class.</param>
+        public static void DuckCopyShallow(object dst, object src)
+        {
+            var srcT = src.GetType();
+            var dstT = dst.GetType();
+            foreach (var f in srcT.GetFields())
+            {
+                var dstF = dstT.GetField(f.Name);
+                if (dstF == null)
+                    continue;
+                dstF.SetValue(dst, f.GetValue(src));
+            }
+
+            foreach (var f in srcT.GetProperties())
+            {
+                var dstF = dstT.GetProperty(f.Name);
+                if (dstF == null)
+                    continue;
+
+                dstF.SetValue(dst, f.GetValue(src, null), null);
+            }
+        }
     }
 }
 
