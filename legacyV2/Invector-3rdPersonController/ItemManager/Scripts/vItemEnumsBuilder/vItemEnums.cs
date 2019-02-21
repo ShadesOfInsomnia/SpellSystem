9bb9eb62a91fd97d9ef9175cf27175eaa7ad7100
Rob Alexander <mr_rob_alexander@protonmail.com>
2019-02-21 15:38:12 +0000

    depricated
    depricated old code, doesnt work with latest invector

---
+++ legacyV2/Invector-3rdPersonController/ItemManager/Scripts/vItemEnumsBuilder/vItemEnums.cs
@@ -0,0 +1,32 @@
+namespace Invector.vItemManager {
+     public enum vItemType {
+       Consumable=0,
+       MeleeWeapon=1,
+       Shooter=2,
+       Ammo=3,
+       Archery=4,
+       Spell=999
+     }
+     public enum vItemAttributes {
+       Health=0,
+       Stamina=1,
+       Damage=2,
+       StaminaCost=3,
+       DefenseRate=4,
+       DefenseRange=5,
+       AmmoCount=6,
+       MaxHealth=7,
+       MaxStamina=8,
+       SecundaryAmmoCount=9,
+       SecundaryDamage=10,
+       MagicID=900,
+       ManaCost=901,
+       MinRange=902,
+       MaxRange=903,
+       Intelligence=904,
+       Strength=905,
+       Dexterity=906,
+       Wisdom=907,
+       Charisma=908
+     }
+}
